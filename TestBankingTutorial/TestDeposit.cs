62b3526710427e1d0e5ad109904fb571e1b68d24
Josh Scott <joshscott82@gmail.com>
2022-08-30 20:23:57 -0400

    Add project files.

---
+++ TestBankingTutorial/TestDeposit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankingTutorial;
+
+namespace TestBankingTutorial
+{
+    public class TestDeposit
+    {
+        Account depositlib;
+
+        public TestDeposit()
+        {
+            depositlib = new Account();
+        }
+
+        [Theory]
+        [InlineData(100, 200, 300)]
+        public void Test1(int Amount, int Balance, int expected)
+        {
+            Assert.Equal(expected, depositlib.Deposit(bool));
+        }
+    }
+}
